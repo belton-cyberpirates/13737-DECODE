ad9122fdcf55c9b5b095e10e871cef2e9b796366
Oshawhat <85427862+Oshawhaat@users.noreply.github.com>
2025-12-08 02:34:24 -0600

    add ActionSequence action and integrate it into far blue auto

--- AutoBlueFar.java
+++ AutoBlueFar.java
@@ -14,15 +14,13 @@ public class AutoBlueFar extends Auto {
 
     public Action[] getActions() {
         
-        Action[] actions = {
-            // ======================= AUTO START ======================= //
-            
+        Action[] launchSequence = {
             // Get ready for launching
             new OpenStopper(this),
             new SpinLauncherFast(this),
             
             // Move to shooting position
-            new Move(this, 180, 0, -90-22.5),
+            new Move(this, 180, 0, -90-24),
             
             // Launch!
             new WaitForLauncher(this),
@@ -38,8 +36,15 @@ public class AutoBlueFar extends Auto {
             // Reset
             new StopLauncher(this),
             new StopIntake(this),
-            new StopPusher(this),
-
+            new StopPusher(this)
+        };
+        
+        Action[] actions = {
+            // ======================= AUTO START ======================= //
+            
+            // Launch!
+            new ActionSequence(this, launchSequence),
+            
             // Move to first line
             new Move(this, 680, 300, -90),
 
@@ -50,36 +55,14 @@ public class AutoBlueFar extends Auto {
             new Move(this, 680, 1100, -90),
             new Wait(this, 500),
             new StopPusher(this),
-            
-            // Get ready for launching
-            new OpenStopper(this),
-            new SpinLauncherFast(this),
-
-            // Move back to shooting position, while intaking to not lose artifacts
-            new Move(this, 180, 0, -90-22.5), // same as first shooting position
-            
-            // Launch!
-            new WaitForLauncher(this),
-            new Wait(this, 500),
-            new SpinPusher(this),
-            new Wait(this, 500),
-            new SpinIntake(this, -.2),
-            new Wait(this, 300),
-            new SpinIntake(this),
-            
-            new Wait(this, 3500),
-
-            // Reset
-            new StopLauncher(this),
             new StopIntake(this),
-            new StopPusher(this),
+            
+            new ActionSequence(this, launchSequence),
 
             // If we have more time 
 
             // Move to second line
             new Move(this, 1280, 300, -90),
-
-            // Intake artifacts
 
             // Intake artifacts
             new CloseStopper(this),
@@ -88,25 +71,9 @@ public class AutoBlueFar extends Auto {
             new Move(this, 1250, 1100, -90),
             new Wait(this, 500),
             new StopPusher(this),
+            new StopIntake(this),
             
-            // Get ready for launching
-            new OpenStopper(this),
-            new SpinLauncherFast(this),
-
-            // Move back to shooting position, while intaking to not lose artifacts
-            new Move(this, 180, 0, -90-22.5), // same as first shooting position
-
-            
-            // Launch!
-            new WaitForLauncher(this),
-            new Wait(this, 500),
-            new SpinPusher(this),
-            new Wait(this, 500),
-            new SpinIntake(this, -.2),
-            new Wait(this, 300),
-            new SpinIntake(this),
-            
-            new Wait(this, 3500),
+            new ActionSequence(this, launchSequence),
 
             // I don't think we have any chance of shooting these artifacts, if we even have time to grab them
             
