7e0b1911663bdc09af257f4579f7ae9c462776e5
Oshawhat <85427862+Oshawhaat@users.noreply.github.com>
2026-01-09 09:07:21 -0600

    things

--- Move.java
+++ Move.java
@@ -8,6 +8,7 @@ public class Move extends Action {
     double xPos;
     double yPos;
     double heading;
+    double speedMult = 1;
     double expectedTime = 0;
     ElapsedTime startTime;
     
@@ -18,23 +19,49 @@ public class Move extends Action {
         this.yPos = yPos;
         this.heading = heading;
     }
-
-    public Move(Auto auto, double xPos, double yPos, double heading, double expectedTime) {
+    
+    
+    public Move(Auto auto, double xPos, double yPos, double heading, double speedMult) {
         this.auto = auto;
         
         this.xPos = xPos;
         this.yPos = yPos;
         this.heading = heading;
+        this.speedMult = speedMult;
+    }
+    
+    
+    public Move(Auto auto, double xPos, double yPos, double heading, double speedMult, double expectedTime) {
+        this.auto = auto;
+        
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.heading = heading;
+        this.speedMult = speedMult;
         this.expectedTime = expectedTime;
     }
+        
+
+    // public Move(Auto auto, double xPos, double yPos, double heading, double expectedTime) {
+    //     this.auto = auto;
+        
+    //     this.xPos = xPos;
+    //     this.yPos = yPos;
+    //     this.heading = heading;
+    //     this.expectedTime = expectedTime;
+    // }
 
     public void onStart() {
         startTime = new ElapsedTime();
-        auto.driveMotors.Move(xPos, yPos, heading);
+        auto.driveMotors.Move(xPos, yPos, heading, this.speedMult);
     }
     
     public boolean isDone() {
         return auto.driveMotors.isDone() || (expectedTime != 0 && startTime.time() >= expectedTime);
+    }
+
+    public void process() {
+        auto.telemetry.addData("Waiting for base...", startTime.time() - expectedTime);
     }
 
 }
